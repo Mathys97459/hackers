384a2ed3cbf7e9726e7f7a9789dd444a9a99a83a
Mathys Dezitter <mathysdezitter.pro@gmail.com>
2024-12-06 09:08:02 +0100

    werewolf to hackers

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,13 +9,13 @@ const anybody = Anybody({
 });
 
 export const metadata = {
-  title: "Werewolf",
+  title: "Hackers",
   description: "",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="fr">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
